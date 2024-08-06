939b590cad1009c2e4965598d0a71fe57bd8394d
boomermath <boomermath13@gmail.com>
2024-08-05 20:43:57 -0400

    makeitneuro landing page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Make it Neuro";
+export const SITE_DESCRIPTION = "A youth organization dedicated to neuroscience.";
+
+export const INSTAGRAM_URL = "https://www.instagram.com/makeitneuro/";
+export const NEWSLETTER_URL = "";
